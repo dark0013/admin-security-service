0abbc65120bbbebb25dfffaa1fca99af3731f979
dark0013 <andresarcangel00@gmail.com>
2026-02-01 19:04:07 -0500

    configuración: conección a la base de datos y estructura de carpeta

---
+++ src/main/java/com/darkross/adminsecurityservice/AdminSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.darkross.adminsecurityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSecurityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminSecurityServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darkross/adminsecurityservice/AdminSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkross.adminsecurityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminSecurityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
